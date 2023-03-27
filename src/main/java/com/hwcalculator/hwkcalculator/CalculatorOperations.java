f4c745679b6c1c434eafe2a0ead9c94bf21b674f
shigaboba <shigapovalrm@gmail.com>
2023-03-27 22:36:39 +0300

    HW is done

--- src/main/java/com/hwcalculator/hwkcalculator/CalculatorOperations.java
+++ src/main/java/com/hwcalculator/hwkcalculator/CalculatorOperations.java
@@ -45,5 +45,4 @@ public class CalculatorOperations {
         }
         else return "Введите оба параметра";
     }
-
 }
---
+++ src/test/java/com/hwcalculator/hwkcalculator/HwkcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwcalculator.hwkcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwkcalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
